fff1a85bfeed426423357cb6a5bfde4924260a1e
sof-ilm-tech <sofilm.tech@gmail.com>
2020-12-15 11:33:01 +0500

    firestore project migration

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -4,7 +4,6 @@ import 'firebase/firestore'
 firebase.initializeApp({
 	apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
 	authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-	databaseURL: import.meta.env.VITE_FIREBASE_DATABASE_URL,
 	projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
 	appId: import.meta.env.VITE_FIREBASE_APP_ID,
 })
