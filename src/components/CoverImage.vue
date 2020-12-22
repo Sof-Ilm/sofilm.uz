c819b7ce758afed40e7fe9fae2b39151686fbfba
sof-ilm-tech <sofilm.tech@gmail.com>
2020-12-22 10:55:04 +0500

    fix cover border

--- src/components/CoverImage.vue
+++ src/components/CoverImage.vue
@@ -2,14 +2,14 @@
 	<div
 		class="rounded border bg-white bg-no-repeat bg-center"
 		:style='{
-			width: `${width}px`,
-			height: `${height}px`,
+			width: `${width+2}px`,
+			height: `${height+2}px`,
 			backgroundSize: "60%",
 			backgroundImage: src ? "none" : `url("/@/assets/podcast.svg")`
 		}'>
 		<div
 			v-if="src"
-			class="bg-no-repeat bg-center bg-cover"
+			class="bg-no-repeat bg-center bg-cover rounded"
 			:style='{
 				width: `${width}px`,
 				height: `${height}px`,
