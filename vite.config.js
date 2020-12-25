568dc24e470a1ea0d855d7b3ca55e0b705adedf6
Azamat Sharapov <azamat.jobcontact@gmail.com>
2020-12-25 17:47:57 +0500

    enable PWA

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,28 @@
-var path = require('path')
+import path from 'path'
+import { VitePWA } from 'vite-plugin-pwa'
 
 module.exports = {
+	plugins: [
+		VitePWA({
+			manifest: {
+				'name': 'Sof Ilm',
+				'short_name': 'Sof Ilm',
+				'start_url': '/',
+				'display': 'standalone',
+				'background_color': '#fff',
+				'description': 'sofilm.uz',
+				'lang': 'uz',
+				'icons': ['192x192', '512x512'].map(size => {
+					return {
+						'src': `/images/logo-${size}.png`,
+						'sizes': size,
+						'type': 'image/png'
+					}
+				})
+			},
+			workbox: {},
+		})
+	],
 	alias: {
 		'/@/': path.resolve(__dirname, 'src')
 	}
