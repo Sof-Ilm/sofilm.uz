f57130d407b48972b1bffff41bd5720e55d8c1aa
Azamat Sharapov <azamat.jobcontact@gmail.com>
2020-12-25 18:41:14 +0500

    edit manifest

--- vite.config.js
+++ vite.config.js
@@ -7,18 +7,28 @@ module.exports = {
 			manifest: {
 				'name': 'Sof Ilm',
 				'short_name': 'Sof Ilm',
-				'start_url': '/',
+				'start_url': '/audio-kitoblar/fiqh',
 				'display': 'standalone',
 				'background_color': '#fff',
 				'description': 'sofilm.uz',
 				'lang': 'uz',
-				'icons': ['192x192', '512x512'].map(size => {
-					return {
-						'src': `/images/logo-${size}.png`,
-						'sizes': size,
+				'icons': [
+					{
+						'src': '/favicon.ico',
+						'sizes': '64x64 32x32 24x24 16x16',
+						'type': 'image/x-icon'
+					},
+					{
+						'src': '/images/logo-192x192.png',
+						'sizes': '192x192',
+						'type': 'image/png'
+					},
+					{
+						'src': '/images/logo-512x512.png',
+						'sizes': '512x512',
 						'type': 'image/png'
 					}
-				})
+				]
 			},
 			workbox: {},
 		})
