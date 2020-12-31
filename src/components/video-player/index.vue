237371efbc8d5e1486cece1d718c8eee91a59681
Azamat Sharapov <azamat.jobcontact@gmail.com>
2020-12-31 16:36:47 +0500

    fix syntax err

--- src/components/video-player/index.vue
+++ src/components/video-player/index.vue
@@ -6,7 +6,7 @@
 					v-if="currentVideo"
 					width="100%"
 					height="100%"
-					:src=`https://www.youtube.com/embed/${currentVideo.id}?rel=0`
+					:src="`https://www.youtube.com/embed/${currentVideo.id}?rel=0`"
 					frameborder="0"
 					allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
 					allowfullscreen>
