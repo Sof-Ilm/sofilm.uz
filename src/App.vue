8823f5c1b16e157a09de327ee5f40ca1d8f0c888
sof-ilm-tech <sofilm.tech@gmail.com>
2021-01-03 11:02:51 +0500

    lazy load players

--- src/App.vue
+++ src/App.vue
@@ -1,41 +1,37 @@
 <template>
-	<div class="xl:container xl:mx-auto relative flex flex-col min-h-screen bg-white border-l border-r">
-		<header class="flex justify-between mx-4 md:mx-10 mt-5">
-			<div class="logo mt-1 md:mt-0"></div>
+	<header class="flex justify-between mx-4 md:mx-10 mt-5">
+		<div class="logo mt-1 md:mt-0"></div>
 
-			<div class="flex justify-between flex-1 self-start pb-2 md:pb-0 md:pt-2 border-b">
-				<MainNav />
-				<SearchForm class="w-1/3 self-start" />
-			</div>
-		</header>
+		<div class="flex justify-between flex-1 self-start pb-2 md:pb-0 md:pt-2 border-b">
+			<MainNav />
+			<SearchForm class="w-1/3 self-start" />
+		</div>
+	</header>
 
-		<main class="flex-1 mx-4 md:mx-10">
-			<router-view></router-view>
-		</main>
+	<main class="flex-1 mx-4 md:mx-10">
+		<router-view></router-view>
+	</main>
 
-		<AudioPlayer v-if="audioAlbumLoaded" class="sticky left-0 right-0 bottom-0 bg-white" />
+	<AudioPlayer v-if="audioAlbumLoaded" class="sticky left-0 right-0 bottom-0 bg-white" />
 
-		<teleport v-if="videoAlbumLoaded" to="body">
-			<VideoPlayer @back-click="unloadVideoAlbum" />
-		</teleport>
-	</div>
+	<teleport v-if="videoAlbumLoaded" to="body">
+		<VideoPlayer @back-click="unloadVideoAlbum" />
+	</teleport>
 </template>
 
 <script>
 import { useStore } from 'vuex'
-import { computed } from 'vue'
+import { computed, defineAsyncComponent } from 'vue'
 import SearchForm from '/@/components/SearchForm.vue'
 import MainNav from '/@/components/MainNav.vue'
-import AudioPlayer from '/@/components/audio-player/index.vue'
-import VideoPlayer from '/@/components/video-player/index.vue'
 
 export default {
 	name: 'App',
 	components: {
 		MainNav,
 		SearchForm,
-		AudioPlayer,
-		VideoPlayer,
+		AudioPlayer: defineAsyncComponent(() => import('/@/components/audio-player/index.vue')),
+		VideoPlayer: defineAsyncComponent(() => import('/@/components/video-player/index.vue')),
 	},
 	setup () {
 		const store = useStore()
