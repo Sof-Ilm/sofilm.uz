c0d3b9145906fb3e1f24ea6e673a44b6b3000069
sof-ilm-tech <sofilm.tech@gmail.com>
2021-03-19 11:23:15 +0500

    Support descriptionId field on albums

--- src/store/modules/album.js
+++ src/store/modules/album.js
@@ -23,6 +23,13 @@ export default {
 			const albumsSnap = await db.collection('albums').where('categories', 'array-contains', category).get()
 			const albums = albumsSnap.docs.map(a => ({id: a.id, category, ...a.data()}))
 
+			for (const alb of albums) {
+				if (alb.descriptionId) {
+					const description = await db.collection('content').doc(alb.descriptionId).get()
+					alb.description = description.data()?.text
+				}
+			}
+
 			commit('addAlbums', {albums})
 		},
 	}
